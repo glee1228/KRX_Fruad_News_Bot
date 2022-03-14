d393d0f6d9055fa5bdc1de8d23d67df017401924
Donghoon Lee <glee1228@naver.com>
2022-03-15 06:10:09 +0900

    *

---
+++ README.md
@@ -0,0 +1,15 @@
+# KRX Fraud News Bot
+
+Example of Python Serverless Telegram bot to be used with <https://vercel.com>
+
+## Steps
+
+### Add env variable to vercel.com
+
+Add your telegram bot token as `BOT_TOKEN` variable
+
+### Register webhook
+
+``` bash
+curl "https://api.telegram.org/bot<BOT_TOKEN>/setWebhook?url=https://your-project-name.vercel.app/api/webhook/"
+```
--- helpers/bot.py
+++ helpers/bot.py
@@ -3,12 +3,6 @@ import re
 
 from telegram.ext import Dispatcher, CommandHandler, CallbackQueryHandler
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
-import os
-import logging
-
-from flask import Flask, Blueprint, request, jsonify
-from telegram import Bot, Update
-
 
 buttons = [
     [
@@ -116,27 +110,3 @@ def get_dispatcher(bot):
     dispatcher.add_handler(CallbackQueryHandler(button_press))
 
     return dispatcher
-
-app = Flask(__name__)
-api = Blueprint("serverless_handler", __name__)
-bot = Bot(os.environ['BOT_TOKEN'])
-app.config["tg_bot"] = bot
-app.config["tg_dispatcher"] = get_dispatcher(bot)
-logger = logging.getLogger(__name__)
-
-
-@api.route("/", methods=["POST"])
-def webhook():
-    update_json = request.get_json()
-    logger.info("input to function %s", update_json)
-    update = Update.de_json(update_json, app.config["tg_bot"])
-    app.config["tg_dispatcher"].process_update(update)
-    return jsonify({"status": "ok"})
-
-
-@api.route("/")
-def home():
-    return "Hi there"
-
-
-app.register_blueprint(api, url_prefix="/api/webhook")
